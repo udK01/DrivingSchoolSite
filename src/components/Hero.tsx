55f1338d79b80b37b774b0698665571472c9ec03
udK01 <dominik.muranyi@gmail.com>
2024-08-29 13:02:25 +0100

    Redesigned site in Figma.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,3 @@
 export default function Hero() {
-  return (
-    <section className="h-screen">
-      <img
-        src="./herocar.jpg"
-        alt="Hero Car"
-        className="top-0 object-cover inset-0 h-screen w-full"
-      />
-    </section>
-  );
+  return <section className="h-screen"></section>;
 }
