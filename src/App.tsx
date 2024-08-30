f328ac05484175b72e0bc90f375f8124cbc3fb18
udK01 <dominik.muranyi@gmail.com>
2024-08-30 15:44:08 +0100

    Made new header based on Figma design.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 
 export default function App() {
   return (
-    <section className="h-screen bg-black">
-      <Navbar />
-      <Hero />
+    <section className="h-screen">
+      <Header />
+      {/* <Navbar />
+      <Hero /> */}
     </section>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <div className="w-full bg-[#2D2D2D] flex justify-center">
+      <div className="w-[80%] h-[124px]  flex items-center">
+        <img src="./logo.png" alt="Iwan Williams Logo" />
+        <div className="bg-red-500 w-full h-full"></div>
+      </div>
+    </div>
+  );
+}
