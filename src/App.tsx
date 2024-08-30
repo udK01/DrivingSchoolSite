64193d2e8d8f90ae61be548bb51f390747be5f07
udK01 <dominik.muranyi@gmail.com>
2024-08-31 00:08:31 +0100

    Added steps class.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Steps from "./components/Steps";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Header />
       <Navbar />
       <Hero />
+      <Steps />
     </section>
   );
 }
