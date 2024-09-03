dc18f636f357303a6a214635e52c28e9b6ec3d25
udK01 <dominik.muranyi@gmail.com>
2024-09-03 19:37:53 +0100

    Added road 1 signs.

--- src/components/Steps.tsx
+++ src/components/Steps.tsx
@@ -9,11 +9,18 @@ export default function Steps() {
         />
       </div>
 
-      <img
-        src="./GetStarted/Road1.png"
-        alt="Get Started Title"
-        className="w-full h-full"
-      />
+      <div className="relative w-full h-full">
+        <img
+          src="./GetStarted/Road1.png"
+          alt="Get Started Title"
+          className="w-full h-full"
+        />
+        <img
+          src="./GetStarted/Signs1.png"
+          alt="Signs"
+          className="absolute top-0 left-0 w-full h-full"
+        />
+      </div>
     </section>
   );
 }
