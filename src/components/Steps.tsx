6b8c4b70a6519f9f959c900278290de355000cc1
udK01 <dominik.muranyi@gmail.com>
2024-09-04 05:12:46 +0100

    Added links, need higher quality image.

--- src/components/Steps.tsx
+++ src/components/Steps.tsx
@@ -1,26 +1,36 @@
 export default function Steps() {
   return (
     <section className="relative">
-      <div className="flex justify-center">
-        <img
-          src="./GetStarted/Title.png"
-          alt="Get Started Title"
-          className="z-10 absolute mt-10 w-[85%]"
-        />
-      </div>
-
-      <div className="relative w-full h-full">
-        <img
-          src="./GetStarted/Road1.png"
-          alt="Get Started Title"
-          className="w-full h-full"
-        />
-        <img
-          src="./GetStarted/Signs1.png"
-          alt="Signs"
-          className="absolute top-0 left-0 w-full h-full"
-        />
-      </div>
+      <img
+        src="./GetStarted/FullRoad.png"
+        alt="Background.png"
+        className="w-full h-full"
+      />
+      {/* Provisional License */}
+      <a
+        href="https://www.gov.uk/apply-first-provisional-driving-licence"
+        className="w-[12%] h-[2%] absolute top-[29.5%] left-[15.5%] hover:cursor-pointer"
+      />
+      {/* Study For Theory */}
+      <a
+        href="https://www.freeonlinetheorytests.co.uk/"
+        className="w-[6.5%] h-[1.8%] absolute top-[48.5%] left-[20.5%] hover:cursor-pointer"
+      />
+      {/* Book Theory */}
+      <a
+        href="https://www.gov.uk/book-theory-test"
+        className="w-[6.5%] h-[1.8%] absolute top-[48.5%] left-[28.8%] hover:cursor-pointer"
+      />
+      {/* Schedule Lessons */}
+      <a
+        href=""
+        className="w-[14%] h-[2%] absolute top-[72.5%] left-[69%] hover:cursor-pointer"
+      />
+      {/* Book Practical */}
+      <a
+        href="https://www.gov.uk/book-theory-test"
+        className="w-[16%] h-[1.8%] absolute top-[94.8%] left-[30%] hover:cursor-pointer"
+      />
     </section>
   );
 }
