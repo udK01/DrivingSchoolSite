fe98d1326d11e77e9e6e28f021fd451606524017
udK01 <dominik.muranyi@gmail.com>
2024-09-15 21:50:07 +0100

    Applied changes to Hero.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -16,7 +16,7 @@ export default function Hero() {
       </div>
 
       <img
-        src="./HeroImg.png"
+        src="./HeroImg.webp"
         alt="Hero-Img"
         className="w-full h-full object-cover absolute inset-0 z-0"
       />
