a1a6a0c3d2926caf2609179e1a8344354d9700a5
udK01 <dominik.muranyi@gmail.com>
2024-09-16 20:30:27 +0100

    Added basic routing.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,40 @@
-import Locations from "./components/Locations";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Header from "./components/Header";
+
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Steps from "./components/Steps";
 import Hero from "./components/Hero";
+import Steps from "./components/Steps";
+import Locations from "./components/Locations";
+
+import Pricing from "./components/Pricing";
+import Instructor from "./components/Instructor";
+
+import Footer from "./components/Footer";
+
+const Home = () => (
+  <>
+    <Hero />
+    <Steps />
+    <Locations />
+  </>
+);
 
 export default function App() {
   return (
-    <section>
+    <Router>
       <Header />
       <Navbar />
-      <Hero />
-      <Steps />
-      <Locations />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about-us" element={<div>About Us Page</div>} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/become-an-instructor" element={<Instructor />} />
+        <Route path="/testimonials" element={<div>Testimonials Page</div>} />
+        <Route path="/contact-us" element={<div>Contact Us Page</div>} />
+      </Routes>
       <Footer />
-    </section>
+    </Router>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,27 +1,30 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
   const options = [
-    "HOME",
-    "ABOUT US",
-    "PRICING",
-    "BECOME AN INSTRUCTOR",
-    "TESTIMONIALS",
-    "CONTACT US",
+    { text: "HOME", link: "/home" },
+    { text: "ABOUT US", link: "/about-us" },
+    { text: "PRICING", link: "/pricing" },
+    { text: "BECOME AN INSTRUCTOR", link: "/become-an-instructor" },
+    { text: "TESTIMONIALS", link: "/testimonials" },
+    { text: "CONTACT US", link: "/contact-us" },
   ];
 
   return (
     <div className="w-full flex justify-center bg-primary_yellow sticky top-0 z-50">
       <div className="w-[80%] h-[60px] flex items-center gap-16 mx-[32px]">
         {options.map((option, index) => (
-          <p
+          <Link
             key={index}
+            to={option.link}
             className="h-full flex items-center font-robot text-[20px] font-black text-primary_gray hover:cursor-pointer hover:text-primary_red transition-all duration-300"
           >
-            {option}
-          </p>
+            {option.text}
+          </Link>
         ))}
-        <p className="flex w-[280px] h-full justify-center items-center text-[20px] font-black font-robot text-white bg-primary_red">
+        <div className="flex w-[280px] h-full justify-center items-center text-[20px] font-black font-robot text-white bg-primary_red">
           SCHEDULE LESSONS
-        </p>
+        </div>
       </div>
     </div>
   );
