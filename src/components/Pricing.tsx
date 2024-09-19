43532231cbf47fb72045cedc104b8acb619e69f2
udK01 <dominik.muranyi@gmail.com>
2024-09-19 17:44:18 +0100

    Added more prices, made price into component.

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -1,13 +1,26 @@
 export default function Pricing() {
+  interface PriceComponentProps {
+    text: string;
+    price: string | number;
+  }
+
+  const PriceComponent = ({ text, price }: PriceComponentProps) => {
+    return (
+      <div className="flex font-poppins w-full items-end border-dotted border-b-[4px] border-primary_red">
+        <div>{text}</div>
+        <div className="flex-grow mx-2"></div>
+        <div>{price}</div>
+      </div>
+    );
+  };
+
   return (
     <section className="flex flex-col items-center">
       <div className="text-[80px] font-bold font-roboto">Prices</div>
       <div className="text-[25px] w-[50%] flex flex-col justify">
-        <div className="flex font-poppins w-full items-end">
-          <div>1 Hour Manual</div>
-          <div className="flex-grow border-dotted border-b border-primary_red border-[6px] mx-2"></div>
-          <div>£34.99/hr</div>
-        </div>
+        <PriceComponent text={"1 Hour Manual"} price={"£34.99"} />
+        <PriceComponent text={"10 Hour Manual"} price={"£320.00"} />
+        <PriceComponent text={"30 Hour Manual"} price={"£879.99"} />
       </div>
     </section>
   );
