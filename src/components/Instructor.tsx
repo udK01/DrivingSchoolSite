44592a6ea8fcd181afaebc773e2e7b874ba7d4ee
udK01 <dominik.muranyi@gmail.com>
2024-09-20 20:14:23 +0100

    Added instructor info page.

--- src/components/Instructor.tsx
+++ src/components/Instructor.tsx
@@ -1,3 +1,18 @@
 export default function Instructor() {
-  return <section>Become an instructor...</section>;
+  return (
+    <section>
+      <div className="text-roboto text-[60px] text-center">
+        Become an Instructor!
+      </div>
+
+      <div className="div">
+        <div className="flex-shrink-0">
+          <div className="">Option 1</div>
+        </div>
+        <div className="flex-shrink-0">
+          <div className="">Option 2</div>
+        </div>
+      </div>
+    </section>
+  );
 }
