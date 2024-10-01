1a54b4df72821b45a007edd8812804e12df86c54
udK01 <dominik.muranyi@gmail.com>
2024-10-01 21:02:34 +0100

    Added more to About Us.

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -1,3 +1,10 @@
 export default function AboutUs() {
-  return <section>We are a privately owned company.</section>;
+  return (
+    <section>
+      <div>
+        At Iwan Williams, our goal is to introduce new, yet safe drivers to our
+        roads.
+      </div>
+    </section>
+  );
 }
