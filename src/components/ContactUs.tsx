8689fc4ca92f79dfd6fbd8da7a04b117a5de9e91
udK01 <dominik.muranyi@gmail.com>
2024-10-06 21:04:13 +0100

    Extended contact info.

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -3,6 +3,7 @@ export default function ContactUs() {
     <section>
       <div>Email: iwanwilli@hotmail.com</div>
       <div>Phone: 01792 870718</div>
+      <div>Twitter: IwanWilliamsSOM</div>
     </section>
   );
 }
