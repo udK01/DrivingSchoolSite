6803494e5b16d0a714efece61583ae3330d7fa88
udK01 <dominik.muranyi@gmail.com>
2024-10-08 23:10:59 +0100

    Fixed typo, extended instructor info.

--- src/components/Instructor.tsx
+++ src/components/Instructor.tsx
@@ -98,7 +98,9 @@ export default function Instructor() {
 
       <div>30 hours at £32.50 = £975</div>
       <div>
-        Extras: Fuel - £80, Franchise Fee - £130. You must suply your own car!
+        Extras: Fuel - £80, Franchise Fee - £130. You must supply your own car!
+        We provide learners, you do the scheduling. Making sure you leave enough
+        time between learners is crucial!
       </div>
     </section>
   );
