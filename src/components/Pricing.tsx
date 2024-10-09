2e3a0e25c70ca3ae9ca138e5b024fe60e9b0990f
udK01 <dominik.muranyi@gmail.com>
2024-10-09 19:18:41 +0100

    Added automatic prices.

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -15,12 +15,17 @@ export default function Pricing() {
   };
 
   return (
-    <section className="flex flex-col items-center">
+    <section className="flex flex-col items-center space-y-10">
       <div className="text-[80px] font-bold font-roboto">Prices</div>
       <div className="text-[25px] w-[50%] flex flex-col justify">
         <PriceComponent text={"1 Hour Manual"} price={"£34.99"} />
         <PriceComponent text={"10 Hour Manual"} price={"£320.00"} />
         <PriceComponent text={"30 Hour Manual"} price={"£879.99"} />
+      </div>
+      <div className="text-[25px] w-[50%] flex flex-col justify">
+        <PriceComponent text={"1 Hour Automatic"} price={"£37.99"} />
+        <PriceComponent text={"10 Hour Automatic"} price={"£370.00"} />
+        <PriceComponent text={"30 Hour Automatic"} price={"£999.99"} />
       </div>
     </section>
   );
