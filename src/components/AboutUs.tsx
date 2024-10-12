01f8a918068be49a84704b32357edb3a13fe9ea9
udK01 <dominik.muranyi@gmail.com>
2024-10-12 22:10:22 +0100

    Moved info to the right place.

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -3,7 +3,8 @@ export default function AboutUs() {
     <section>
       <div>
         At Iwan Williams, our goal is to introduce new, yet safe drivers to our
-        roads. We are active in Neath Port Talbot, Swansea and Llanelli.
+        roads. We are active in Neath Port Talbot, Swansea and Llanelli. We
+        provide male/female smoking-free instructors.
       </div>
     </section>
   );
--- src/components/Instructor.tsx
+++ src/components/Instructor.tsx
@@ -100,8 +100,7 @@ export default function Instructor() {
       <div>
         Extras: Fuel - £80, Franchise Fee - £130. You must supply your own car!
         We provide learners, you do the scheduling. Making sure you leave enough
-        time between learners is crucial! We provide male/female smoking-free
-        instructors.
+        time between learners is crucial!
       </div>
     </section>
   );
