5c7b9bd7a817442de44dd1af21a8d668695957f3
udK01 <dominik.muranyi@gmail.com>
2024-10-15 21:25:53 +0100

    Added extra about me.

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -4,7 +4,8 @@ export default function AboutUs() {
       <div>
         At Iwan Williams, our goal is to introduce new, yet safe drivers to our
         roads. We are active in Neath Port Talbot, Swansea and Llanelli. We
-        provide male/female smoking-free instructors.
+        provide male/female smoking-free instructors. We've been part of The Top
+        3 Driving Schools in Swansea multiple times in a row!
       </div>
       <img src="./IwanReward.jpeg" />
     </section>
