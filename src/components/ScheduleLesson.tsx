a0f00fc4e7eef3f394214777ff6f2c5205142e69
udK01 <dominik.muranyi@gmail.com>
2024-10-19 22:34:19 +0100

    Added schedule button.

--- src/components/ScheduleLesson.tsx
+++ src/components/ScheduleLesson.tsx
@@ -1,3 +1,9 @@
 export default function ScheduleLesson() {
-  return <div>Lesson</div>;
+  return (
+    <section>
+      <button className="bg-red-500 p-2 rounded-2xl hover:bg-red-800">
+        Schedule
+      </button>
+    </section>
+  );
 }
